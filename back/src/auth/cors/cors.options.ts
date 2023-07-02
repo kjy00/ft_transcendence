79ad2e5a414602c4be8fb06fac31886775aa9013
donghyuk <reg0145@naver.com>
2023-07-02 18:26:16 +0900

    [Refactor] 패키지명 변경 (auth -> security)

--- back/src/auth/cors/cors.options.ts
+++
@@ -1,8 +0,0 @@
-import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
-
-export const corsOptions: CorsOptions = {
-  origin: 'http://localhost:3000',
-  methods: 'GET, POST',
-  credentials: false,
-  maxAge: 3600,
-};
