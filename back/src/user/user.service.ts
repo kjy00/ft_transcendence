1d83a71f5aca8846243226c0c0d8ddb1afd3499c
donghyuk <reg0145@naver.com>
2023-07-02 15:43:59 +0900

    [Feat] 유저 회원가입(Signup) 구현하기

--- back/src/user/user.service.ts
+++ back/src/user/user.service.ts
@@ -1,4 +1,22 @@
-import { Injectable } from '@nestjs/common';
-
+import { Injectable, Inject } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
+import { bcrypt } from 'bcrypt';
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @Inject('USER_REPOSITORY') private userRepository: Repository<User>,
+  ) {}
+  async signup(nickname: string, password: string, email: string) {
+    const user = await this.userRepository.findOne({ where: { nickname } });
+    if (user) {
+      throw new Error('이미 존재하는 사용자입니다.');
+    }
+    const hashedPassword = await bcrypt.hash(password, 12);
+    await this.userRepository.save({
+      nickname,
+      hashedPassword,
+      email,
+    });
+  }
+}
