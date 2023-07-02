9e52e8787bce381e3c5eb79bc3d5f3187c63db11
donghyuk <reg0145@naver.com>
2023-07-02 18:26:16 +0900

    [Refactor] 패키지명 변경 (auth -> security)

--- back/src/main.ts
+++ back/src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { corsOptions } from './auth/cors/cors.options';
+import { corsOptions } from './security/cors/cors.options';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
